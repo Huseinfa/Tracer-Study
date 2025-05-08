3722deb954a674eb4e7bf726ebd2d3b277cc70d8
AlKhrisW <aldokhrisna@gmail.com>
2025-05-08 15:08:21 +0700

    unsliced adminlte

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,44 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class UserModel extends Authenticatable
+class User extends Authenticatable
 {
-    use HasFactory;
-
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-
-    protected $hidden = ['password'];
-
-    protected $casts = ['password' => 'hashed'];
-
-    public function level(): BelongsTo 
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
-     /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string
-    {
-        return $this->level->level_nama;
-    }
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * Cek apakah user memiliki role tertentu
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
      */
-    public function hasRole($role): bool
-    {
-        return $this->level->level_kode == $role;
-    }
-    /**
-     * Mendapatkan kode role
-     */
-    public function getRole()
-    {
-        return $this->level->level_kode;
-    }
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
