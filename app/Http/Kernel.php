e7c0be51580ce1f9801eb15fc7cbc46f10f6bc1a
AlKhrisW <aldokhrisna@gmail.com>
2025-06-06 11:15:56 +0700

    feat & fix: search list, update email, middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'search' => \App\Http\Middleware\Search::class,
-        'verifikasiLulusan' => \App\Http\Middleware\VerifikasiSudahMengisi::class,
         'otpLulusan' => \App\Http\Middleware\OtpLulusan::class,
     ];
 }
--- app/Http/Middleware/Search.php
+++ app/Http/Middleware/Search.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\LulusanModel;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +13,23 @@ class Search
     {
         if (!session()->has('id_lulusan')) {
             return redirect()->route('tracer-study.index');
+        } else {
+            if (session('sudah_mengisi')) {
+                if ($request->wantsJson() || $request->ajax()) {
+                    session()->flush();
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Anda sudah pernah mengisi tracer study.',
+                        'redirect_url' => route('tracer-study.thanks')
+                    ]);
+                } else {
+                    session()->flush();
+                    return redirect()->route('tracer-study.thanks')
+                        ->with('error', 'Anda sudah pernah mengisi tracer study.');
+                }
+            } 
         }
+
         return $next($request);
     }
 }
